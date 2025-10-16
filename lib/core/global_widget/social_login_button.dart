06bf813ed55bb99533a39cef3a357d95451ec0f7
Tanvir <tanvirhasanemn@gmail.com>
2025-10-16 15:23:36 +0600

    init3

--- lib/core/global_widget/social_login_button.dart
+++ lib/core/global_widget/social_login_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../utility/app_colors.dart';
 
 class SocialLoginButton extends StatelessWidget {
--- lib/features/profile/presentation/screen/profile.dart
+++ lib/features/profile/presentation/screen/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../../../../core/global_widget/custombuttonwhite.dart';
 import '../../../../core/utility/app_colors.dart';
 import '../../controller/profile_controller.dart';
 
@@ -138,8 +139,13 @@ class ProfilePage extends StatelessWidget {
 
                         ],
                       ),
+SizedBox(height: 24.h,),
+                      CustomButtonWhite(
+                        text:'Edit Profile',
+                      onPressed: (){
 
-
+                      },
+                      ),
                       SizedBox(height: 40.h),
 
                       // About Me Section
